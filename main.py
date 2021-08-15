f73a3b92cb9dfa9f1526f0c01fe5a8a4924ac628
uglkjgj <32646097+uglkjgj@users.noreply.github.com>
2021-08-15 19:47:34 +0100

    use environment variables

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from random import shuffle
 app = Flask(__name__)
 Bootstrap(app)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hippo-stock.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
